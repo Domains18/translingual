1c70c0312f3bb92eaeab3285b78d41edd5265102
domains18 <gibsonsgibson88@gmail.com>
2022-12-24 01:03:04 -0500

    main javascript

--- client/script.js
+++ client/script.js
@@ -1,2 +1,27 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chat_container = document.querySelector('#chat_container');
+let loadInterval;
+
+function loader(element){
+    element.textContent = '';
+    loadInterval = setInterval(()=>{
+        element.textContent += '.';
+        if (element.textContent.length > 4){
+            element.textContent = "";
+        }
+    }, 300)
+}
+
+function typeText(element, text){
+    let i = 0;
+    let interval = setInterval(()=>{
+        element.textContent += text[i];
+        i++;
+        if (i >= text.length){
+            clearInterval(interval);
+        }
+    }, 20)
+}
